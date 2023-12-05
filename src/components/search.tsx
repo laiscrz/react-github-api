f3e4e156b736df0908631943a828ebcb4f98fd2d
laiscrz <lais.albino2003@gmail.com>
2023-12-05 15:14:36 -0300

    Add função handleKeyDown para evento de tecla

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -2,7 +2,7 @@ type SearchProps = {
     loadUser : (userName : string) => Promise<void>
 }
 
-import { useState } from 'react';
+import { useState, KeyboardEvent } from 'react';
 
 import { BsSearch } from 'react-icons/bs';
 
@@ -11,13 +11,25 @@ import classes from "./search.module.css"
 const Search = ({loadUser} : SearchProps) => {
     const [userName,setUserName] = useState("");
     
+    const handleKeyDown = (e : KeyboardEvent) =>{
+        if(e.key === "Enter"){
+            loadUser(userName)
+        }
+    }
+
+
+
     return (
         <div className={classes.search}>
             <h2>Busque por um usuário</h2>
             <p>Conheça seus melhores repositorios</p>
             <div className={classes.input_group}>
-                <input type="text" placeholder="Digite o nome do usuário" 
-                onChange={(e) => setUserName(e.target.value)}/>
+                <input 
+                type="text" 
+                placeholder="Digite o nome do usuário" 
+                onChange={(e) => setUserName(e.target.value)}
+                onKeyDown={handleKeyDown}
+                />
                 <button onClick={() => loadUser(userName)}>
                     <BsSearch />
                 </button>
