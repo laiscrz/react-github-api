ef7d2bfe0159d364e8fb24fa281a913171de6164
laiscrz <lais.albino2003@gmail.com>
2023-12-05 15:09:10 -0300

    add css module

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 
+import classes from "./App.module.css";
+
 function App() {
 
 
   return (
     <>
-      <div className="app">
-        <h1>Consumo Api Github</h1>
+      <div className={classes.app}>
+        <h1>Github Finder</h1>
         <Outlet />
       </div>
     </>
--- src/components/search.tsx
+++ src/components/search.tsx
@@ -6,14 +6,16 @@ import { useState } from 'react';
 
 import { BsSearch } from 'react-icons/bs';
 
+import classes from "./search.module.css"
+
 const Search = ({loadUser} : SearchProps) => {
     const [userName,setUserName] = useState("");
     
     return (
-        <div>
+        <div className={classes.search}>
             <h2>Busque por um usuário</h2>
             <p>Conheça seus melhores repositorios</p>
-            <div>
+            <div className={classes.input_group}>
                 <input type="text" placeholder="Digite o nome do usuário" 
                 onChange={(e) => setUserName(e.target.value)}/>
                 <button onClick={() => loadUser(userName)}>
