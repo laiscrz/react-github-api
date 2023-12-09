6aa48468d8c9c6ab44d813acee0028547e2c895b
laiscrz <lais.albino2003@gmail.com>
2023-12-09 11:16:42 -0300

    add componente repo

--- src/routes/repos.tsx
+++ src/routes/repos.tsx
@@ -1,8 +1,18 @@
 
-const Repos = () =>{
-    return(
+import { useParams } from "react-router-dom"
+
+
+const Repos = () => {
+    const { username } = useParams();
+
+    
+
+    return (
         <div>
-            
+            <h2>Explore os repositórios do usuário : {username}</h2>
+            <div>
+                
+            </div>
         </div>
     )
 }
