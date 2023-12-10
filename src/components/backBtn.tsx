3d56fe41701a49280ede1dea8e55e057654bbba7
laiscrz <lais.albino2003@gmail.com>
2023-12-10 10:46:44 -0300

    add componente backBtn

---
+++ src/components/backBtn.tsx
@@ -0,0 +1,16 @@
+import { useNavigate } from "react-router-dom"
+
+const BackBtn = () =>{
+    const navigate = useNavigate();
+
+
+    return(
+        <>
+            <button onClick={() => navigate(-1)}>
+                Voltar
+            </button>
+        </>
+    )
+}
+
+export default BackBtn
--- src/routes/repos.tsx
+++ src/routes/repos.tsx
@@ -5,54 +5,56 @@ import Loader from "../components/loader";
 import Repo from "../components/repo";
 
 import classes from "./repos.module.css"
+import BackBtn from "../components/backBtn";
 
 const Repos = () => {
-    const { username } = useParams();
+  const { username } = useParams();
 
-    const [repos, setRepos] = useState<RepoProps[] | [] | null>(null);
+  const [repos, setRepos] = useState<RepoProps[] | [] | null>(null);
 
-    const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
-        const loadrepos = async function (username: string) {
-            setIsLoading(true);
+  useEffect(() => {
+    const loadrepos = async function (username: string) {
+      setIsLoading(true);
 
-            const res = await fetch(`https://api.github.com/users/${username}/repos`)
+      const res = await fetch(`https://api.github.com/users/${username}/repos`)
 
-            const data = await res.json();
+      const data = await res.json();
 
-            setIsLoading(false);
+      setIsLoading(false);
 
-            let orderedRepos = data.sort(
-                (a: RepoProps, b: RepoProps) => b.stargazers_count - a.stargazers_count
-            );
-
-            orderedRepos = orderedRepos.slice(0, 5);
-
-            setRepos(orderedRepos);
-        };
-
-        if (username) {
-            loadrepos(username);
-        }
-
-    }, [])
-
-
-    if (!repos && isLoading) return <Loader />
-
-    return (
-        <div className={classes.repos}>
-          <h2>Explore os repositórios do usuário: {username}</h2>
-          {repos && repos.length === 0 && <p>Não há repositórios.</p>}
-          {repos && repos.length > 0 && (
-            <div className={classes.repos_container}>
-              {repos.map((repo: RepoProps) => (
-                <Repo key={repo.name} {...repo} />
-              ))}
-            </div>
-          )}
-        </div>
+      let orderedRepos = data.sort(
+        (a: RepoProps, b: RepoProps) => b.stargazers_count - a.stargazers_count
       );
+
+      orderedRepos = orderedRepos.slice(0, 5);
+
+      setRepos(orderedRepos);
     };
+
+    if (username) {
+      loadrepos(username);
+    }
+
+  }, [])
+
+
+  if (!repos && isLoading) return <Loader />
+
+  return (
+    <div className={classes.repos}>
+      <BackBtn />
+      <h2>Explore os repositórios do usuário: {username}</h2>
+      {repos && repos.length === 0 && <p>Não há repositórios.</p>}
+      {repos && repos.length > 0 && (
+        <div className={classes.repos_container}>
+          {repos.map((repo: RepoProps) => (
+            <Repo key={repo.name} {...repo} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
 export default Repos
